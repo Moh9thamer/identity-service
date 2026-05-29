4c97f2db0c851cb6acf73d524460aad4cf12b663
Mohammed Thamer <moh9thamer@gmail.com>
2026-05-29 22:05:42 +0300

    feat: add user and refresh token entities

---
+++ src/Identity.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,9 @@
+namespace Identity.Domain.Entities;
+
+public class RefreshToken : BaseEntity
+{
+    public Guid UserId { get; set; }
+    public string Token { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+    public DateTime? RevokedAt { get; set; }
+}
