e6fde7961b4aa24fe4928dbb26a40d42bcff6566
Moh9thamer <124746433+Moh9thamer@users.noreply.github.com>
2026-06-13 13:16:39 +0300

    add FluentValidation for register and login requests (#7)

---
+++ src/Identity.Application/Validators/Auth/LoginRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Identity.Application.DTOs.Auth;
+
+namespace Identity.Application.Validators.Auth
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotEmpty();
+        }
+    }
+}
